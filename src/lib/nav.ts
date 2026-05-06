dcb639eed9d6bfb4241e2c9bd05270b313267323
ahmadKamran145 <ahmad.kamran@sprintx.net>
2026-05-06 10:37:20 +0500

    change the links

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -7,8 +7,6 @@ export const NAV_LINKS = [
 ];
 
 export const SOCIAL_LINKS = [
-  { href: "https://instagram.com", label: "Instagram" },
-  { href: "https://vimeo.com", label: "Vimeo" },
-  { href: "https://youtube.com", label: "YouTube" },
-  { href: "https://linkedin.com", label: "LinkedIn" },
+  { href: "https://www.instagram.com/rabtfilms/", label: "Instagram" },
+  { href: "https://youtube.com/@rabtfilms?si=8QqhfGonDzVQi-DU", label: "YouTube" },
 ];
